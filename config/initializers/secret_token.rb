254ebdcc222b6363339b343ac277e7298f563f29
anfreey <anfreey@gmail.com>
2012-12-20 13:23:32 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nicepro::Application.config.secret_token = 'd3f15e9dfe2ba6e6e3e78a141b651b72e0dbaf431489386e38831de7b8ead88a3a3be1b2ce6f872d10bc6d7d8a6872589de56c1d981c5e41ef8cede5355ced6d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Nicepro::Application.config.session_store :cookie_store, key: '_nicepro_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Nicepro::Application.config.session_store :active_record_store
